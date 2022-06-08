2d94b639ccfc3f8115b41b5c08a9cc367dd56374
mohamed <mohamedaydi30@gmail.com>
2022-06-08 22:15:07 +0100

    update project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashbordComponent } from './dashbord/dashbord.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { HospitalComponent } from './hospital/hospital.component';
 import { RequestsManagementComponent } from './requests-management/requests-management.component';
+import { SectorsComponent } from './sectors/sectors.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:"",component:DashbordComponent},
-  {path:"requestsManegement",component:RequestsManagementComponent}
+  {path:"dashbord",component:DashbordComponent},
+  {path:"requests",component:RequestsManagementComponent},
+  {path:"sector",component:SectorsComponent},
+  {path:"hospital",component:HospitalComponent},
+  {path:"doctor",component:DoctorComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"",component:SigninComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RequestsManagementComponent } from './requests-management/requests-management.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { SectorsComponent } from './sectors/sectors.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { HospitalComponent } from './hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashbordComponent,
     NavbarComponent,
-    RequestsManagementComponent
+    RequestsManagementComponent,
+    SignupComponent,
+    SigninComponent,
+    SectorsComponent,
+    DoctorComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule,
